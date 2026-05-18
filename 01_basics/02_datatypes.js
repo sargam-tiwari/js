77acc2ff7fcbef690c1c0dc039270492a0b5c247
sargam-tiwari <connect.sargam.tiwari@gmail.com>
2026-05-18 19:45:13 +0000

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code as newer version
+//alert(3+3) we are using nodejs, not browser
+/*
+number => 2 to power 53
+bigint
+string=> ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned
+symbol => unique
+object*/
+let name="sargam"
+console.log(typeof null)
